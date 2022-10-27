843493aa0de7be296b9b178b2b5f8e5916a0862a
Domen Yuta <s21c1085ep@s.chibakoudai.jp>
2022-10-27 12:28:30 +0900

    test.bash

--- test.bash
+++ test.bash
@@ -1,3 +1,17 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2022 Yuta Domen
+# SPDX-License-Identifier: BSD-3-Clause
 
-seq 5 | ./plus
+ng() {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+out=$(seq 5 | ./plus)
+[ "${out}" = 14 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
+
